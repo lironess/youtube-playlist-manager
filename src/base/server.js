158da3b6b9c0c56ca3d92d57048a7ca4f14011c1
Liron Shmuel <liri33@gmail.com>
2015-10-27 12:08:54 +0200

    use default process port for heroku

--- src/base/server.js
+++ src/base/server.js
@@ -4,7 +4,7 @@ import Routes from './routes';
 
 const server = new Hapi.Server();
 
-server.connection({port: Config.port});
+server.connection({port: process.env.PORT || Config.port});
 server.route(Routes.endpoints);
 server.start(() => {
   console.log('Server running at: ', server.info.uri);
