89943ae5be6c85eeb09d1f7e61e401d04e9e887b
Liron Shmuel <liri33@gmail.com>
2015-10-27 22:41:31 +0200

    add cors

--- src/base/server.js
+++ src/base/server.js
@@ -6,7 +6,10 @@ const server = new Hapi.Server({
   connections: { router: { stripTrailingSlash: true } }
 });
 
-server.connection({port: process.env.PORT || Config.port});
+server.connection({
+  port: process.env.PORT || Config.port,
+  routes: { cors: true }
+});
 server.route(Routes.endpoints);
 server.start(() => {
   console.log('Server running at: ', server.info.uri);
